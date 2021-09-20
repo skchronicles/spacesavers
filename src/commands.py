381b33b90da3e111bf1776daea33e445ab075364
skchronicles <kuhnsa2@vcu.edu>
2021-09-20 17:34:54 -0400

    Adding function to convert user/group ids to user/group names

--- src/commands.py
+++ src/commands.py
@@ -48,6 +48,64 @@ def readable_size(sbytes):
     return size
 
 
+def _name(uid, uid_type):
+    """Handler to name() to get the name of a given id. If a uid/gid of 
+    an inactive account is provided the original uid or gid is returned.
+    @param uid_type <int>:
+        Type of identifer, either 'user' or 'group'.
+    @params uid_records <dict>:
+        Lookup of previously encountered uid/gid.
+    @return name <str>:
+        Returns the name of the user_id or group_id
+    """
+    try:
+        # Search for id in the unix database
+        if uid_type == 'user':
+            name = getpwuid(uid).pw_name
+        elif uid_type == 'group':
+            name = getgrgid(uid).gr_name
+    except KeyError:
+        # The uid or gid does not exist in unix database.
+        # This could be an old user or group that does not
+        # exist anymore; however, the file will still 
+        # will use the uid or gid in listings with ls.
+        # Example:
+        # -rw-rw---- 1 39452 CCBR 24931746426 Feb  5  2020 ./rawdata/file.bam
+        name = uid 
+    return name
+
+
+def name(uid, uid_type, uid_records):
+    """Converts a user_id/group_id into a user_name/group_name while maintaining 
+    a local lookup of previously converted ids to prevent redundant unix database search.
+    Using a local cache or lookup is about twice as fast and prevents unneccesary hammering of
+    the unix user/group database. 'uids' of inactive/deleted users or groups cannot be converted
+    so the uid will be returned as the user/group name. 
+    @param uid <int>:
+        Unique identifer for a user or group.
+    @param uid_type <int>:
+        Type of identifer, either 'user' or 'group'.
+    @params uid_records <dict>:
+        Lookup of previously encountered uid/gid.
+    @return name <str>:
+        Returns the name of the user_id or group_id 
+    """
+    try:
+        # Try searching records/cache of previous lookups
+        # to prevent hammering of unix group database.
+        # This method is about twice as fast as just
+        # converting every id we encounter.
+        name = uid_records[uid]
+    except KeyError:
+        # The uid or gid is not in our records,
+        # search the unix group database and add 
+        # it to the record of encountered ids.
+        name = _name(uid, uid_type) 
+        uid_records[uid] = name
+
+    return name 
+
+
 def _ls(path, md5=False):
     """Private function for spacesavers ls() which recursively lists
     information about files and directories for a given path.
@@ -59,7 +117,9 @@ def _ls(path, md5=False):
     # Normalize path
     path = normalized(path)
 
-    # Convert userids to groupids
+    # Keeps track of previously converte user/group
+    # ids to avoid redundant lookups in the unix 
+    # user/group database. 
     users = {}
 
     # Recursively descend the directory tree
@@ -78,12 +138,16 @@ def _ls(path, md5=False):
                 # Possible errors inlcude 
                 err('WARNING: Failed to get info on "{}" due to "{}" error!'.format(file, e))
                 continue   # goto next file
-            
+
+            # Get the file's permissions, inode reference, 
+            # owner and group name, modified timestamp, and 
+            # size of the file in bytes and a human readable
+            # format.
             mode = stat_res.st_mode
             permissions = stat.filemode(mode)
             inode = stat_res.st_ino
-            owner = stat_res.st_uid  # TODO: convert id to name later
-            group = stat_res.st_gid  # TODO: convert id to name later
+            owner = name(stat_res.st_uid, 'user', users)
+            group = name(stat_res.st_gid, 'group', users)
             mdate = datetime.datetime.fromtimestamp(stat_res.st_mtime).strftime('%Y-%m-%d-%H:%M')
             bsize = stat_res.st_size
             hsize = readable_size(bsize)
