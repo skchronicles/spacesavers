d2cf87c5aa1b7c796714d900c970bbd8bb6e681b
skchronicles <kuhnsa2@vcu.edu>
2021-09-30 18:03:35 -0400

    Redirect timers message to standard error

--- src/benchmark.py
+++ src/benchmark.py
@@ -5,10 +5,13 @@
 from __future__ import print_function
 import time
 
+# Local imports
+from utils import err
+
 
 def timer(func):
     """Decorator that calculates how long a function takes to run.
-    The elapsed time is printed to standard output.
+    The elapsed time is printed to standard error stream.
     @param func <func>:
         Function to time
     """
@@ -19,11 +22,11 @@ def timer(func):
         result = func(*args, **kw)
         # End time
         te = time.time()
-        print('{}\t{} ms'.format(func.__name__, (te - ts) * 1000)) 
+        err('{}\t{} ms'.format(func.__name__, (te - ts) * 1000)) 
         return result
     return timed
 
 
 if __name__ == '__main__':
     # Add test later
-    print('Generating a random string of length 64: {}'.format(random_string()))
+    pass
